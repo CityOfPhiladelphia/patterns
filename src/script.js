05466070cd950b49c35e3dea559f07ff7b44a07d
Karissa <k@karissademi.com>
2015-01-30 09:59:16 -0500

    code view fix

--- src/script.js
+++ src/script.js
@@ -2,8 +2,7 @@ $(function() {
   $(".the-code").hide();
 
   $(".code").click(function () {
-      $(this).next(".the-code").toggle("fast");
-      //$("i").removeClass("fa-arrow-circle-right");
-      $("i").toggleClass("fa-arrow-circle-down").toggleClass("fa-arrow-circle-right");
+      $(this).next("i").next(".the-code").toggle("fast");
+      $(this).next("i").toggleClass("fa-arrow-circle-down").toggleClass("fa-arrow-circle-right");
   });
 });
