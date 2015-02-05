87bb7ca097e1f894a01f858f22916a4733659ccb
Karissa <k@karissademi.com>
2015-02-05 12:09:22 -0500

    add ids

--- src/script.js
+++ src/script.js
@@ -6,4 +6,16 @@ $(function() {
       $(this).find("i").toggleClass("fa-arrow-circle-down").toggleClass("fa-arrow-circle-right");
   });
 
+  $("#toggle-side-nav").click(function(e) {
+    if ($(this).attr("href") == "#")
+        e.preventDefault();
+
+    $(".container").toggleClass("navigating");
+    $("ul.side-nav").toggleClass("expanded");
+  });
+
+  $(".side-nav a").click(function(e) {
+      $("ul.side-nav").toggleClass("expanded");
+      $(".container").toggleClass("navigating");
+  });
 });
