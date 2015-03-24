4ca93c4efd96f8093a90ae19ad7f94bc43e9379b
Karissa <k@karissademi.com>
2015-03-24 17:05:51 -0400

    locally scope jQuery

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
-$(function() {
-  //variable footer height for sticky footer
-  var footerHeight = $("footer").height();
-  $("#page").css("padding-bottom", footerHeight);
-  $("#application").css("padding-bottom", footerHeight);
-  $("footer").css("margin-top", -footerHeight);
+jQuery(document).ready(function( $ ) {
+    // You can use the locally-scoped $ in here as an alias to jQuery.
+      //variable footer height for sticky footer
+      var footerHeight = $("footer").height();
+      $("#page").css("padding-bottom", footerHeight);
+      $("#application").css("padding-bottom", footerHeight);
+      $("footer").css("margin-top", -footerHeight);
 });
