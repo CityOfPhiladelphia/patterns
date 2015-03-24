786ae919ca97466e23b0a2144aa11ff08ffa6701
Karissa <k@karissademi.com>
2015-03-24 16:30:57 -0400

    add pattern filter fixes #16

--- src/pattern-script.js
+++ src/pattern-script.js
@@ -6,23 +6,11 @@ $(function() {
       $(this).find("i").toggleClass("fa-arrow-circle-down").toggleClass("fa-arrow-circle-right");
   });
 
-  $("#toggle-side-nav").click(function(e) {
-
-    $(".top-bar").removeClass("expanded");
-
-    if ($(this).attr("href") == "#")
-        e.preventDefault();
-
-    $(".container").toggleClass("navigating");
-    $("ul.side-nav").toggleClass("expanded");
-  });
-
-  $(".side-nav a").click(function(e) {
-      $("ul.side-nav").toggleClass("expanded");
-      $(".container").toggleClass("navigating");
-  });
-  $(".toggle-topbar").click(function(e){
-    $(".side-nav").removeClass("expanded");
-    $(".container").removeClass("navigating");
-  });
 });
+
+var options = {
+  valueNames: [ 'title' ],
+  searchClass: 'filter'
+};
+
+var userList = new List('pattern-search', options);
