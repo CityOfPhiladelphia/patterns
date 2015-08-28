c2aacf860ecfd1bbc275a39fe845510126f2a0d6
Karissa Demi <k@karissademi.com>
2015-08-28 13:15:02 -0400

    Let Javascript handle hover state

--- src/pattern-script.js
+++ src/pattern-script.js
@@ -6,7 +6,16 @@ $(function() {
       $(this).find("i").toggleClass("fa-arrow-circle-down").toggleClass("fa-arrow-circle-right");
   });
   // this is here so it works properly in the all patterns view
-  $(".story.s-box").click(function() {
+
+  var mediaBox = $(".story.s-box");
+
+  mediaBox.hover( function() {
+    $( this ).addClass( 'lower-opacity' );
+  }, function() {
+    $( this ).removeClass( 'lower-opacity' );
+  });
+
+  mediaBox.click(function() {
     window.location = $(this).find("a").attr("href");
     return false;
   });
--- src/script.js
+++ src/script.js
@@ -1,22 +1,29 @@
 jQuery(document).ready(function( $ ) {
-    // You can use the locally-scoped $ in here as an alias to jQuery.
-      //variable footer height for sticky footer
-      var footerHeight = $("footer").height();
-      $("#page").css("padding-bottom", footerHeight);
-      $("#application").css("padding-bottom", footerHeight);
-      $("footer").css("margin-top", -footerHeight);
+  // You can use the locally-scoped $ in here as an alias to jQuery.
+  //variable footer height for sticky footer
 
-      //show/hide the footer on modal open/close to prevent breakage
-      $(document).on('open.fndtn.reveal', '[data-reveal]', function () {
-        $('footer.site-footer').hide();
-      });
+  var footerHeight = $("footer").height();
+  $("#page").css("padding-bottom", footerHeight);
+  $("#application").css("padding-bottom", footerHeight);
+  $("footer").css("margin-top", -footerHeight);
 
-      $(document).on('close.fndtn.reveal', '[data-reveal]', function () {
-        $('footer.site-footer').show();
-      });
+  //show/hide the footer on modal open/close to prevent breakage
+  $(document).on('open.fndtn.reveal', '[data-reveal]', function () {
+    $('footer.site-footer').hide();
+  });
 
-      $(".story.s-box").click(function() {
-        window.location = $(this).find("a").attr("href");
-        return false;
-      });
+  $(document).on('close.fndtn.reveal', '[data-reveal]', function () {
+    $('footer.site-footer').show();
+  });
+
+  var mediaBox = $(".story.s-box");
+  mediaBox.hover( function() {
+    $( this ).addClass( 'lower-opacity' );
+  }, function() {
+    $( this ).removeClass( 'lower-opacity' );
+  });
+  mediaBox.click(function() {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
 });
