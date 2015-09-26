76c10e18043989e2594b16a9d3530edfeebe0cbf
Karissa Demi <k@karissademi.com>
2015-09-26 14:02:36 -0400

    Hide code blocks on load

--- src/pattern-script.js
+++ src/pattern-script.js
@@ -1,6 +1,4 @@
 $(function() {
-  $(".the-code").hide();
-
   $(".code").click(function () {
       $(this).next(".the-code").toggle("fast");
       $(this).find("i").toggleClass("fa-arrow-circle-down").toggleClass("fa-arrow-circle-right");
