b88707a2c9625478579608b1e0063762d4dc02d2
Karissa Demi <k@karissademi.com>
2015-09-30 13:05:16 -0400

    Remove old directories

--- 0.3.0/js/patterns.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  //variable footer height for sticky footer
-  var footerHeight = $("footer").height();
-  $("#page").css("padding-bottom", footerHeight);
-  $("#application").css("padding-bottom", footerHeight);
-  $("footer").css("margin-top", -footerHeight);
-});
