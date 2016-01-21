0478e1c9812abded493f87fee483a5f6d4e25258
Karissa Demi <k@karissademi.com>
2016-01-21 16:00:18 -0500

    Use Grunt to compile sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,32 +5,71 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     uglify: {
-       options: {
-         sourceMap: true
-       },
-       pattern_js: {
-         files: {
-           'js/dist/patterns.min.js':
-           ['bower_components/filament-tablesaw/dist/stackonly/tablesaw.stackonly.js',
-           'bower_components/nprogress/nprogress.js',
-           'js/src/patterns.js'
-         ]
-       }
-     }
-   },
+      options: {
+        sourceMap: true
+      },
+      pattern_js: {
+        files: {
+          'js/dist/patterns.min.js':
+          [
+            'bower_components/filament-tablesaw/dist/stackonly/tablesaw.stackonly.js',
+            'bower_components/nprogress/nprogress.js',
+            'js/src/patterns.js'
+          ]
+        }
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          loadPath: [
+            'bower_components/foundation-sites/scss',
+            'bower_components/motion-ui/src'
+          ],
+          sourcemap: 'auto'
+        },
+        files: {
+          'css/patterns.css': '_scss/patterns/patterns.scss',
+          'css/styleguide.css': '_scss/styleguide/styleguide.scss'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: true,
+          processors: [
+            require('autoprefixer')({
+              browsers: ['last 2 versions']
+            })
+          ]
+        },
+      dist: {
+        src: 'css/*.css'
+      }
+    },
+
     watch: {
       js: {
         files: ['js/src/*.js'],
         tasks: ['uglify']
+      },
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
       }
     }
   });
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'watch']);
+  grunt.registerTask('default', ['uglify', 'watch', 'scss']);
 
 };
