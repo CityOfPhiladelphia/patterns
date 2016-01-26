082f9e58c8f46e312ccf8fdbff8dd5dc34493a61
Karissa Demi <k@karissademi.com>
2016-01-26 17:17:55 -0500

    Updated js

--- js/styleguide-script.js
+++ js/styleguide-script.js
@@ -7,6 +7,20 @@ $(function() {
       $(this).find("i").toggleClass("fa-arrow-circle-down").toggleClass("fa-arrow-circle-right");
   });
 
+  $("nav ul.children").hide();
+
+  $( window ).scroll(function() {
+
+    if ( $("nav .children a").hasClass("active") ) {
+
+      var current = $("nav .children a.active");
+
+      $( current ).parent().parent().show();
+
+    }
+
+  });
+
 });
 
 new Clipboard('.copy');
