dc20a7224c46ff26b74f23209f522d75a6bcde42
Karissa Demi <k@karissademi.com>
2016-02-09 17:15:10 -0500

    Keep it simple, goodbye sticky footer

--- js/src/patterns.js
+++ js/src/patterns.js
@@ -22,24 +22,10 @@ jQuery(document).ready(function( $ ) {
   if ($('.has-tip').length > 0) {
 
     var tooltip = new Foundation.Tooltip($('.has-tip'), options);
-  
+
   }
 
   //reponsive tables
   $( document ).trigger( "enhance.tablesaw" );
-
-  //variable footer height for sticky footer
-  var footerHeight = $("footer.site-footer").height();
-  $("#page").css("padding-bottom", footerHeight);
-  $("#application").css("padding-bottom", footerHeight);
-  $("footer.site-footer").css("margin-top", -footerHeight);
-
-  //show/hide the footer on modal open/close to prevent breakage
-  $(document).on('open.fndtn.reveal', '[data-reveal]', function () {
-    $('footer.site-footer').hide();
-  });
-
-  $(document).on('close.fndtn.reveal', '[data-reveal]', function () {
-    $('footer.site-footer').show();
-  });
+  
 });
