501e7572b01e8be7d8faf08eec6b0e6847df722a
Karissa Demi <k@karissademi.com>
2016-04-06 16:51:47 -0400

    Don't use include, because IE

--- js/src/patterns.js
+++ js/src/patterns.js
@@ -38,7 +38,7 @@ jQuery(document).ready(function( $ ) {
 
     var loc = [location.protocol, '//', location.host, location.pathname].join('');
 
-    if ( $(this).attr('href').includes('dept=') ) {///we know it's coming from alpha
+    if ( $(this).attr('href').indexOf('dept=') ) {///we know it's coming from alpha
 
       var url = $(this).attr('href') + '&url=' + loc;
 
